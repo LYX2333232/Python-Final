c5b1013486d788021a8ac8aa8d49a9e782f315c2
223 <l15723746653@outlook.com>
2023-11-03 16:26:58 +0800

    创建基础的window界面

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print(123)
+import tkinter as tk
+
+window = tk.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+# Label
+label = tk.Label(text="I am a Label", font=("Arial", 24, "bold"))
+label.pack()
+
+window.mainloop()
