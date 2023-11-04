cf07057fd92158b2f28fad4c7ed1216064301525
223 <l15723746653@outlook.com>
2023-11-04 15:04:49 +0800

    修改文件布局

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import tkinter as tk
-from openFile import openFile
-from saveFile import saveFile
-from welcome import Welcome
+from FILE.openFile import openFile
+from FILE.saveFile import saveFile
+from pages.welcome import Welcome
 
-if __name__ == "__main__":
+
+def welcomePage():
     window = tk.Tk()
     window.title("文件阅读器")
     window.minsize(width=500, height=300)
     welcome = Welcome(openFile=openFile, saveFile=saveFile, quit=window.quit)
     welcome.pack(window)
     window.mainloop()
+
+
+if __name__ == "__main__":
+    welcomePage()
---
+++ pages/txtReader.py
@@ -0,0 +1,4 @@
+# txt文件阅读器
+class TxtReader:
+    def __init__(self, path):
+        self.path = path
